414e2af80277f7fdc773cdaac1fa1502a58c3d15
Thao Nguyen <thao.nguyen@zaplabs.com>
2019-11-02 19:50:24 -0700

    Initial Commit

---
+++ iOsNote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOsNote
+//
+//  Created by Thao Nguyen on 11/2/19.
+//  Copyright © 2019 Thao Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
